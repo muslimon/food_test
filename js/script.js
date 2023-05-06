6db283ad8111f0b53640ec15aa3149f9050af37f
Mus <81385968+muslimon@users.noreply.github.com>
2023-05-06 22:24:34 +0300

    включение модального окна при прокрутке страницы и с течением времени

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-
+    //TABS
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items');
@@ -37,7 +37,70 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    //TIMER
+    const deadline = '2023-08-07';
+   
+    function getTimeRemaining(endtime){
+        const t = Date.parse(endtime) - Date.parse(Date());
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor(t / (1000 * 60 * 60) % 24),
+            minutes = Math.floor(t / (1000 * 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+
+              return {
+               'total': t,
+                days,
+                hours,
+                minutes,
+                seconds
+              };
+    }   
+    
+    function getZero(num) {
+        if(num >= 0 && num < 10){
+            return `0${num}`;
+        } else if (num <= 0) {
+            return '00';
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+              updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime); 
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+            
+            if (t.total <=0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
 
 
+    // Modal
+
+    const modalBtn = document.querySelectorAll('[data-modal]'),
+          closeBtn = document.querySelector('[data-close]');
+     
 
 });
+
+
+
+
