78d9a334070b44c0a741f823bda9bbfa72fd36c5
Mus <81385968+muslimon@users.noreply.github.com>
2023-05-23 21:33:51 +0300

    карточки создаются через json

--- js/script.js
+++ js/script.js
@@ -189,37 +189,25 @@ window.addEventListener('DOMContentLoaded', () => {
 			this.parent.prepend(element);
 		}
 	}
+
+	const getResource = async (url) => {     
+		const res = await fetch(url);
+
+		if (!res.ok) {
+			throw new Error(`Could not fetch ${url}, status ${res.status}`);
+		}
+
+		return await res.json();
+	};
+
+	getResource('http://localhost:3000/menu')
+		.then(data => {
+			data.forEach(obj => {
+				obj.parent = '.menu .container';
+				new MenuCard(...Object.values(obj)).render();
+			});
+		});
 	
-	new MenuCard(
-		"img/tabs/vegy.jpg",
-		"vegy",
-		'Меню "Фитнес"',
-		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-		9,
-		".menu .container",
-		"menu__item"
-	).render();
-
-	new MenuCard(
-		"img/tabs/elite.jpg",
-		"elite",
-		'Меню “Премиум”',
-		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-		15,
-		".menu .container",
-		"menu__item"
-	).render();
-
-	new MenuCard(
-		"img/tabs/post.jpg",
-		"post",
-		'Меню "Постное"',
-		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-		13,
-		".menu .container",
-		"menu__item"
-	).render();
-
 
 	//Forms back-end
 
