a363ff684d030fa3720d95bcc9592321e1c23b29
Mus <81385968+muslimon@users.noreply.github.com>
2023-06-01 17:38:37 +0300

    added babel + core.js

--- js/script.js
+++ js/script.js
@@ -34,4 +34,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	});
 
 
+	
+
+
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,35 @@
-/* eslint-disable linebreak-style */
+
 'use strict';
 
 let path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: './js/script.js',
-  output: {
-    filename: 'bundle.js',
-    path: __dirname + '/js'
-  },
-  watch: true,
+	mode: 'production',
+	entry: './js/script.js',
+	output: {
+		filename: 'bundle.js',
+		path: __dirname + '/js'
+	},
+	watch: true,
 
-  devtool: "source-map",
+	devtool: "source-map",
 
-  module: {}
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [['@babel/preset-env', {
+								debug: true,
+								corejs: 3,
+								useBuiltIns: "usage"
+						}]]
+					}
+				}
+			}
+		]
+	}
 };
